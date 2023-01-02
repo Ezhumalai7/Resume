f0971f62409ee9f37f216765a7c5b8a17e93bc05
Ezhumalai7 <121295508+Ezhumalai7@users.noreply.github.com>
2023-01-02 10:43:15 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,36 @@
+var myResume= {
+    "PERSONAL DETAILS":{
+        "NAME":"Ezhumalai p",
+        "DOB":"21-04-2000",
+        "NATIONALITY":"indian",
+        "CONTACT":6369080179,
+        "E-MAIL":"sakthi777ep@gmail.com"
+        },
+        "CAREER OBJECTIVES":{
+            ">":"To work is a organization who provides me with amble opportunity to enhance my skills and knownledge along with contributing to the growth of the organization.",
+        },
+        "SKILLS":{
+            "PROGRAMING LANGUAGE":"javascript,reactjs,nodejs",
+            "WEB DESIGNING":"html,css,bootstrab,wordpress",  
+        },
+        "EDUCATION":{
+            "SSLC":"st.pauls hr sec school,2015,82%",
+            "HSC":"st.pauls hr sec school,2018,70%",
+            "DEGREE":"B.E(cse),2022,80%",
+        },
+        "ACADAMIC PROJECT":{
+            "NAME":"time domain based security and shared bigdata access using multi cloud cryptograpic",
+            "PROJECT DETAILS":"Cloud computing is recognized as an alternative to traditional information technology due to its intrinsic resource sharing and low-maintenance characteristics"
+        
+
+         },
+        "HOBBIES":{
+            "1":"Travelling",
+            "2":"Playing cricket",
+            "3":"playing volley ball",
+        },
+        "DECLARATION":{
+            ">":"I here by declare that information given above is correct and true",
+        },    
+}
+console.log(myResume)
